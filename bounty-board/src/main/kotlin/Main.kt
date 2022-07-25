67447e53175c735b068804b97f20a53a573e7625
adforus_hjp <hjp281@adforus.co.kr>
2022-07-25 12:33:56 +0900

    🚧 TypeInterface 76p까지

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 package main.kotlin
 
 fun main(){
-    println("Hello, world!")
+    println("The hero announces her presence to the world.")
+
+    val heroName = "Madrigal"
+    println(heroName)
+
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel++
+    println(playerLevel)
 }
