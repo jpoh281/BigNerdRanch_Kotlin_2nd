13e29feb7709cceaedf71d7656e63871d6dde394
jpoh281 <hjp4869@naver.com>
2022-08-01 12:41:57 +0900

    ✨널 세이프티 부분 완성

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -10,12 +10,8 @@ fun main() {
     println("$HERO_NAME announces her presence to the world.")
     println("What level is $HERO_NAME?")
 
-    var playerLevelInput = readLine()!!
-    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
-        playerLevelInput.toInt()
-    } else {
-        1
-    }
+    playerLevel = readLine()?.toIntOrNull() ?: 0
+
     println("$HERO_NAME's level is $playerLevel.")
 
     readBountyBoard()
@@ -37,7 +33,7 @@ private fun obtainQuest(
     playerClass: String = "Paladin",
     hasBeFriendedBarbarians: Boolean = true,
     hasAngeredBarbarians: Boolean = false,
-): String = when (playerLevel) {
+): String? = when (playerLevel) {
     1 -> "Meet Mr. Bubbles in the land of soft things."
     in 2..5 -> {
         val canTalkToBarbarians = !hasAngeredBarbarians && hasBeFriendedBarbarians || playerClass == "barbarian"
@@ -51,16 +47,22 @@ private fun obtainQuest(
     6 -> "Locate the enchanted sword."
     7 -> "Recover the long-lost artifact of createion."
     8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-    else -> "There are no quests right now."
+    else -> null
 }
 
 private fun readBountyBoard() {
-    println(
-        """
-        "$HERO_NAME approaches the bounty board. It reads :"
-            "${obtainQuest(playerLevel).replace("Nogartse", "xxxxxxx")}"
-        """.trimIndent()
-    )
+    val quest: String? = obtainQuest(playerLevel)
+
+    val message: String = quest?.replace("Nogartse", "xxxxxxxx")
+        ?.let { censoredQuest ->
+
+            """
+            |$HERO_NAME approaches the bounty board. It reads:
+            |   "$censoredQuest"
+            """.trimMargin()
+        } ?: "$HERO_NAME approaches the bounty board, but it is blank."
+
+    println(message)
 }
 
 fun forgeItem(
