e3db9f085c367ed3c4c73f65dfb1fff029dc525a
jpoh281 <hjp4869@naver.com>
2022-08-24 22:40:29 +0900

    Any -> Generic으로 변경

--- NyetHack/src/main/kotlin/com/hdd/nyethack/Extensions.kt
+++ NyetHack/src/main/kotlin/com/hdd/nyethack/Extensions.kt
@@ -2,7 +2,7 @@ package com.hdd.nyethack
 
 fun String.addEnthusiasm(enthusiasmLevel: Int = 1) = this + "!".repeat(enthusiasmLevel)
 
-fun Any.print() : Any {
+fun <T>T.print() : T {
     println(this)
     return this
 }
