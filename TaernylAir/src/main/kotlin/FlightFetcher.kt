5489a0d813d803dab3d20b8c464ef506e157d6e5
jpoh281 <hjp4869@naver.com>
2022-08-25 23:42:06 +0900

    🚧 진행중

--- TaernylAir/src/main/kotlin/FlightFetcher.kt
+++ TaernylAir/src/main/kotlin/FlightFetcher.kt
@@ -1,13 +1,21 @@
+import kotlinx.coroutines.*
 import java.net.URL
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
 private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
 
 fun main() {
-    println("Started")
-    val flight = fetchFlight()
-    println(flight)
-    println("Finished")
+    runBlocking {
+        println("Start")
+        launch {
+            val flight = fetchFlight()
+            println(flight)
+        }
+        println("Finished")
+
+    }
 }
 
-fun fetchFlight(): String = URL(FLIGHT_ENDPOINT).readText()
+suspend fun fetchFlight(): String = withContext(Dispatchers.IO){
+    URL(FLIGHT_ENDPOINT).readText()
+} 
