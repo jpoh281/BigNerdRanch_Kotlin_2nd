b604597e34c60ff126120c949ed14fce1d51faf4
jpoh281 <hjp4869@naver.com>
2022-08-25 23:06:37 +0900

    🎉 프로젝트 시작

---
+++ TaernylAir/src/main/kotlin/FlightFetcher.kt
@@ -0,0 +1,13 @@
+import java.net.URL
+
+private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
+private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+
+fun main() {
+    println("Started")
+    val flight = fetchFlight()
+    println(flight)
+    println("Finished")
+}
+
+fun fetchFlight(): String = URL(FLIGHT_ENDPOINT).readText()
